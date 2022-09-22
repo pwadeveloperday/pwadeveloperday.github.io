b779c72253263f3affc19839db9f6a6d0e8d3ce4
Belem Zhang <belem.zhang@intel.com>
2022-09-22 15:47:47 +0800

    Updates

--- _app/immutable/chunks/1-e5608ce3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-f3dfa697.js";export{t as component};
---
+++ _app/immutable/chunks/2-3b8df561.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-9fc9820e.js";const e=!0;export{t as component,e as server};
